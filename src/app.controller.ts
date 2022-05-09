19875aab34ae7e13e7bf42ab3ba290ad34715980
Nozomu Hasegawa <hsnznewton7@gmail.com>
2022-05-09 22:13:06 +0900

    install typeorm  module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  @Render('index')
+  root() {
+    return {
+      title: 'Action Goals Manager',
+      msg: 'send form here! :',
+    };
+  }
+
+  @Post('/')
+  send(@Body() form: any) {
+    return form;
   }
 }
